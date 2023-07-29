49d99cabb74fcce7f4804d3849d5fc3f7b96fb24
Sihwan Yu <tlghks1217@naver.com>
2023-07-29 10:39:09 +0900

    init

---
+++ src/main/java/challenge/competer/CompeterApplication.java
@@ -0,0 +1,13 @@
+package challenge.competer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompeterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/challenge/competer/CompeterApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.competer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
