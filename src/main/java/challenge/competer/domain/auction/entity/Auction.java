d8fc6970b3931878ee98b73a69cbd639ccbfb584
princeton <ssk0080@gmail.com>
2023-07-31 13:48:14 +0900

    feat: 경매 엔티티 생성

---
+++ src/main/java/challenge/competer/domain/auction/entity/Auction.java
@@ -0,0 +1,41 @@
+package challenge.competer.domain.auction.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+@Entity(name = "auctions")
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class Auction {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long openingPrice;
+
+    @Column(nullable = false)
+    private Long winningPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime openingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime closingTime;
+
+}
