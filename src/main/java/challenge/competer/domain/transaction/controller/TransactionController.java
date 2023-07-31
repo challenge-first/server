c88fb3b74d4ecf4b4850f2b2e02fa17d918b1365
kki4504 <rhkddlf9876@gmail.com>
2023-07-31 14:50:24 +0900

    feat: create transaction entity three layer

---
+++ src/main/java/challenge/competer/domain/transaction/controller/TransactionController.java
@@ -0,0 +1,9 @@
+package challenge.competer.domain.transaction.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class TransactionController {
+}
