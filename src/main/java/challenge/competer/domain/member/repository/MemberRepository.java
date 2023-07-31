379bcfe2db373a591dcaea589398f2a3b742d599
kki4504 <rhkddlf9876@gmail.com>
2023-07-31 14:47:26 +0900

    feat: create member entity three layer

---
+++ src/main/java/challenge/competer/domain/member/repository/MemberRepository.java
@@ -0,0 +1,4 @@
+package challenge.competer.domain.member.repository;
+
+public interface MemberRepository {
+}
---
+++ src/main/java/challenge/competer/domain/member/service/MemberService.java
@@ -0,0 +1,7 @@
+package challenge.competer.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
