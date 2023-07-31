c9e9d58fbc88ef293e20b0af7f498f4d87c3cce2
Sihwan Yu <tlghks1217@naver.com>
2023-07-31 20:23:40 +0900

    refactor: #9 global dto 수정
    global dto @AllArgsConstructor 추가

--- src/main/java/challenge/competer/global/response/ResponseDataDto.java
+++ src/main/java/challenge/competer/global/response/ResponseDataDto.java
@@ -1,5 +1,6 @@
 package challenge.competer.global.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 public class ResponseDataDto<T> {
 
     private T data;
