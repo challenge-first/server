964a8c7920506055a897af0b1eeaf83a6167d0ef
kki4504 <rhkddlf9876@gmail.com>
2023-08-02 15:05:20 +0900

    feat: #16 Transaction service 구현

--- src/main/java/challenge/competer/domain/transaction/service/TransactionService.java
+++ src/main/java/challenge/competer/domain/transaction/service/TransactionService.java
@@ -1,4 +1,12 @@
 package challenge.competer.domain.transaction.service;
 
+import challenge.competer.domain.transaction.dto.RequestTransactionDto;
+import challenge.competer.domain.transaction.dto.ResponseTransactionDto;
+import challenge.competer.global.auth.MemberDetails;
+import challenge.competer.global.response.ResponseDataDto;
+import challenge.competer.global.response.ResponseMessageDto;
+
 public interface TransactionService {
+    ResponseMessageDto payment(Long productId, RequestTransactionDto requestDto, MemberDetails memberDetails);
+    ResponseDataDto<ResponseTransactionDto> getTransaction(Long productId, MemberDetails memberDetails);
 }
