799c58b20fd22f5ac49d2dac552830bea26f6bba
Dsys1129 <dsys1129@gmail.com>
2023-08-04 14:18:32 +0900

    feat: cors 설정

--- src/main/java/challenge/competer/global/config/WebConfig.java
+++ src/main/java/challenge/competer/global/config/WebConfig.java
@@ -3,13 +3,28 @@ package challenge.competer.global.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import challenge.competer.global.auth.LoginMemberArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*") //모든 헤더 허용
+                .exposedHeaders(HttpHeaders.AUTHORIZATION) //클라이언트가 응답에 접근할 수 있는 헤더 추가
+                .allowCredentials(true) // 모든 인증 정보 허용
+                .maxAge(MAX_AGE_SECS);
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
