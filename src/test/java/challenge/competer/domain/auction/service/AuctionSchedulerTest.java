834d016065065f10bbc8c8c02583a623a82c25d4
Sihwan Yu <tlghks1217@naver.com>
2023-08-16 11:45:21 +0900

    test: #41 Scheduler 등록 테스트 및 테스트코드 리팩토링

--- src/test/java/challenge/competer/domain/auction/service/AuctionSchedulerTest.java
+++ src/test/java/challenge/competer/domain/auction/service/AuctionSchedulerTest.java
@@ -1,33 +1,16 @@
 package challenge.competer.domain.auction.service;
 
-import challenge.competer.domain.auction.entity.Auction;
-import challenge.competer.domain.auction.repository.AuctionRepository;
-import challenge.competer.domain.image.entity.Image;
-import challenge.competer.domain.member.entity.Member;
-import challenge.competer.domain.member.repository.MemberRepository;
-import challenge.competer.domain.member.role.Role;
-import challenge.competer.domain.product.entity.Product;
-import challenge.competer.domain.product.productenum.ProductState;
-import challenge.competer.domain.product.productenum.ProductType;
-import challenge.competer.domain.product.productenum.SubCategory;
-import challenge.competer.domain.product.repository.ImageRepository;
-import challenge.competer.domain.product.repository.ProductRepository;
-import challenge.competer.global.auth.MemberDetails;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTaskHolder;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @SpringBootTest
 @Transactional
