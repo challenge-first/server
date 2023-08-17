bef30db4385856c5be704d175cfadedf2c70c8aa
Sihwan Yu <tlghks1217@naver.com>
2023-08-18 02:20:41 +0900

    test: AuctionRepository 테스트 코드 수정

--- src/test/java/challenge/competer/domain/auction/repository/AuctionRepositoryTest.java
+++ src/test/java/challenge/competer/domain/auction/repository/AuctionRepositoryTest.java
@@ -1,21 +1,20 @@
 package challenge.competer.domain.auction.repository;
 
 import challenge.competer.domain.auction.entity.Auction;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AuctionRepositoryTest {
 
     @Autowired
@@ -25,9 +24,8 @@ public class AuctionRepositoryTest {
     void beforeEach() {
         for (int i = 0; i < 5; i++) {
             Auction auction = Auction.builder()
-                    .id((long) i + 1)
-                    .openingTime(LocalDateTime.now().withHour(15).plusDays(i))
-                    .closingTime(LocalDateTime.now().withHour(16).withMinute(59).plusDays(i))
+                    .openingTime(LocalDateTime.now().withHour(15).withMinute(0).withSecond(0).plusDays(i))
+                    .closingTime(LocalDateTime.now().withHour(16).withMinute(59).withSecond(0).plusDays(i))
                     .productId(1L)
                     .memberId(1L)
                     .openingPrice(100L)
@@ -38,32 +36,25 @@ public class AuctionRepositoryTest {
         }
     }
 
-    @AfterEach
-    void afterEach() {
-        auctionRepository.deleteAll();
-    }
-
     @Test
-    @DisplayName("현재 시간 기준 opening, closing 사이 경매 조회 테스트")
+    @DisplayName("현재 시간 기준 참여가 가능한 경매 조회 테스트")
     public void findByCurrentTimeTest() throws Exception {
         //given, when
         Optional<Auction> byCurrentTime = auctionRepository.findByCurrentTime(LocalDateTime.now().withHour(15));
 
         //then
         assertThat(byCurrentTime.isPresent()).isTrue();
-        assertThat(byCurrentTime.get().getId()).isEqualTo(1L);
         assertThat(byCurrentTime.get().getOpeningTime()).isAfter(LocalDateTime.now().withHour(14).withMinute(59));
         assertThat(byCurrentTime.get().getClosingTime()).isBefore(LocalDateTime.now().withHour(17));
     }
 
     @Test
-    @DisplayName("opening, closing 사이 경매 조회 테스트")
+    @DisplayName("시작, 종료시간 기준 경매 조회 테스트")
     public void findByClosingTimeBetweenTest() throws Exception {
 
         Optional<Auction> byClosingTime = auctionRepository.findByClosingTimeBetween(LocalDateTime.now().withHour(15), LocalDateTime.now().withHour(16).withMinute(59));
 
         assertThat(byClosingTime.isPresent()).isTrue();
-        assertThat(byClosingTime.get().getId()).isEqualTo(1L);
         assertThat(byClosingTime.get().getOpeningTime()).isAfter(LocalDateTime.now().withHour(14).withMinute(59));
         assertThat(byClosingTime.get().getClosingTime()).isBefore(LocalDateTime.now().withHour(17));
     }
